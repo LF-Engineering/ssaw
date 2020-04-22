94c3cca878cb50a3113524163aedff6a01147f4c
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-04-22 11:00:29 +0000

    Triggers updates
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sql/sh_sf_sync_tables.sql
+++ sql/sh_sf_sync_tables.sql
@@ -1,14 +1,16 @@
-drop table if exists uuids_for_sf_sync;
-create table uuids_for_sf_sync(
-  uuid varchar(128) collate utf8mb4_unicode_520_ci not null,
-  last_modified datetime(6) not null default now(),
-  primary key(uuid)
-) engine=InnoDB default charset=utf8mb4 collate=utf8mb4_unicode_520_ci;
-
+-- organizations for the next SF sync
 drop table if exists orgs_for_sf_sync;
 create table orgs_for_sf_sync(
   name varchar(192) collate utf8mb4_unicode_520_ci not null,
   last_modified datetime(6) not null default now(),
   primary key(name)
+) engine=InnoDB default charset=utf8mb4 collate=utf8mb4_unicode_520_ci;
+
+-- users/profiles for the next SF sync
+drop table if exists uuids_for_sf_sync;
+create table uuids_for_sf_sync(
+  uuid varchar(128) collate utf8mb4_unicode_520_ci not null,
+  last_modified datetime(6) not null default now(),
+  primary key(uuid)
 ) engine=InnoDB default charset=utf8mb4 collate=utf8mb4_unicode_520_ci;
 
--- sql/sh_sf_sync_triggers.sql
+++ sql/sh_sf_sync_triggers.sql
@@ -1,19 +1,28 @@
+-- organizations
 drop trigger if exists organizations_insert_sf_sync_trigger;
 drop trigger if exists organizations_update_sf_sync_trigger;
 drop trigger if exists organizations_delete_sf_sync_trigger;
 
+drop trigger if exists domains_organizations_insert_sf_sync_trigger;
+drop trigger if exists domains_organizations_update_sf_sync_trigger;
+drop trigger if exists domains_organizations_delete_sf_sync_trigger;
+
+-- users/profiles
 drop trigger if exists uidentities_insert_sf_sync_trigger;
 drop trigger if exists uidentities_update_sf_sync_trigger;
 drop trigger if exists uidentities_delete_sf_sync_trigger;
 
 delimiter $
 
+-- organizations
+-- organizations table
 create trigger organizations_insert_sf_sync_trigger after insert on organizations
 for each row begin
   insert into orgs_for_sf_sync(name) values(new.name) on duplicate key update last_modified = now();
 end$
 create trigger organizations_update_sf_sync_trigger after update on organizations
 for each row begin
+  insert into orgs_for_sf_sync(name) values(old.name) on duplicate key update last_modified = now();
   insert into orgs_for_sf_sync(name) values(new.name) on duplicate key update last_modified = now();
 end$
 create trigger organizations_delete_sf_sync_trigger after delete on organizations
@@ -21,6 +30,23 @@ for each row begin
   insert into orgs_for_sf_sync(name) values(old.name) on duplicate key update last_modified = now();
 end$
 
+-- domains_organizations table
+create trigger domains_organizations_insert_sf_sync_trigger after insert on domains_organizations
+for each row begin
+  insert into orgs_for_sf_sync(name) (select name from organizations where id = new.organization_id) on duplicate key update last_modified = now();
+end$
+create trigger domains_organizations_update_sf_sync_trigger after update on domains_organizations
+for each row begin
+  insert into orgs_for_sf_sync(name) (select name from organizations where id = old.organization_id) on duplicate key update last_modified = now();
+  insert into orgs_for_sf_sync(name) (select name from organizations where id = new.organization_id) on duplicate key update last_modified = now();
+end$
+create trigger domains_organizations_delete_sf_sync_trigger after delete on domains_organizations
+for each row begin
+  insert into orgs_for_sf_sync(name) (select name from organizations where id = old.organization_id) on duplicate key update last_modified = now();
+end$
+
+-- users/profiles
+-- uidentities table
 create trigger uidentities_insert_sf_sync_trigger after insert on uidentities
 for each row begin
   insert into uuids_for_sf_sync(uuid) values(new.uuid) on duplicate key update last_modified = now();
