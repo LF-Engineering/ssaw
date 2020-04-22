cd781107ec69781e4f9b283f90510789e53c3384
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2020-04-22 10:37:16 +0000

    Unify names
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- sql/sh_sf_triggers.sql
+++ sql/sh_sf_triggers.sql
@@ -1,10 +1,13 @@
 drop trigger if exists organizations_insert_sf_sync_trigger;
 drop trigger if exists organizations_update_sf_sync_trigger;
 drop trigger if exists organizations_delete_sf_sync_trigger;
+
 drop trigger if exists uidentities_insert_sf_sync_trigger;
 drop trigger if exists uidentities_update_sf_sync_trigger;
 drop trigger if exists uidentities_delete_sf_sync_trigger;
+
 delimiter $
+
 create trigger organizations_insert_sf_sync_trigger after insert on organizations
 for each row begin
   insert into orgs_for_sf_sync(name) values(new.name) on duplicate key update last_modified = now();
@@ -17,6 +20,7 @@ create trigger organizations_delete_sf_sync_trigger after delete on organization
 for each row begin
   insert into orgs_for_sf_sync(name) values(old.name) on duplicate key update last_modified = now();
 end$
+
 create trigger uidentities_insert_sf_sync_trigger after insert on uidentities
 for each row begin
   insert into uuids_for_sf_sync(uuid) values(new.uuid) on duplicate key update last_modified = now();
@@ -29,4 +33,5 @@ create trigger uidentities_delete_sf_sync_trigger after delete on uidentities
 for each row begin
   insert into uuids_for_sf_sync(uuid) values(old.uuid) on duplicate key update last_modified = now();
 end$
+
 delimiter ;
